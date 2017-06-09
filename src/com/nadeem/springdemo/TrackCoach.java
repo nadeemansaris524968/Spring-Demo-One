88abef7d0d159662c2b4e849522530685e86afc6
Nadeem Ansari <s524968@mail.nwmissouri.edu>
2017-06-09 18:12:19 -0500

    Add applicationContext.xml for ClassPathXMLApplicationContext
    Add simple Java classes - BaseballCoach and TrackCoach implementing Coach interface.

---
+++ src/com/nadeem/springdemo/TrackCoach.java
@@ -0,0 +1,11 @@
+package com.nadeem.springdemo;
+
+public class TrackCoach implements Coach {
+
+	@Override
+	public String getDailyWorkout() {
+		
+		return "Spend an hour everyday on the track";
+	}
+
+}
